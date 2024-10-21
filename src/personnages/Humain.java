52ec5948bab649503882293d4d874913282f84d5
splat31 <bastien31800@orange.fr>
2024-10-21 19:11:28 +0200

    premier depot: creation de la classe humain et du scenario HistoireTP4 (1-)

---
+++ src/personnages/Humain.java
@@ -0,0 +1,49 @@
+package personnages;
+
+public class Humain {
+	private String nom;
+	private String fav;
+	private int argent;
+
+	public Humain(String nom, String fav,int argent) {
+		this.nom = nom;
+		this.fav = fav;
+		this.argent = argent;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public int getArgent() {
+		return argent;
+	}
+	
+	public void parler(String texte) {
+		System.out.println("("+ nom + ") - " + texte );
+	}
+	public void direBonjour() {
+		this.parler("Bonjour ! Je m’appelle " + nom + " Set j’aime boire du" + fav);
+	}
+	
+	public void boire() {
+		this.parler("Mmmm, un bon verre de" + fav + "! GLOUPS !");
+	}
+	
+	public void acheter(String bien, int prix) {
+		if (prix > argent) {
+			this.parler("Je n'ai plus que "+ argent +" sous en poche. Je ne peux meme pas m'offrir un" + bien + " à " + prix + "sous");
+		} else {
+		    this.parler("J'ai "+argent +" sous en poche. Je vais pouvoir m'orir un " + bien + " à " + prix + " sous ");
+		    perdreArgent(prix);
+		}
+		
+	}
+	
+	public void gagnerArgent(int gain) {
+		
+	}
+	
+	public void perdreArgent(int perte) {
+		
+	}
+
+}
